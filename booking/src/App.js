93b9294e684b6851d47d3030364d293e18b239d0
cgarbs <christian.garba@gmail.com>
2021-11-29 19:34:01 -0700

    done

--- booking/src/App.js
+++ booking/src/App.js
@@ -1,29 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+// { Switch, Route, Redirect }
+
 import Create from './components/Create.js'
+import Reserve from './components/Reserve.js'
 import View from './components/View.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Create />
-      <View />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Create />
+        <View />
+        <Reserve />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ booking/src/components/Reserve.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reserve extends Component {
+    render() {
+        return (
+            <div>
+                MAKE ACTUAL RESERVATION
+            </div>
+        );
+    }
+}
+
+export default Reserve;
